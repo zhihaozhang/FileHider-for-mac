e9e3b6825643734697aa183ba69c63f6f0ec759b
Chih-Hao <zhihaozhang@me.com>
2018-01-11 11:53:52 +0800

    first init

---
+++ FileHider/mainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  mainViewController.swift
+//  FileHider
+//
+//  Created by Chih-Hao on 2018/1/11.
+//  Copyright © 2018年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class mainViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
