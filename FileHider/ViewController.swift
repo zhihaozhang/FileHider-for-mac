1ae2acefa319f9d93935882f6a33b9d321b53a95
zhihao zhang <zhihaozhang@me.com>
2018-01-11 11:45:58 +0800

    Initial Commit

---
+++ FileHider/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileHider
+//
+//  Created by Chih-Hao on 2018/1/11.
+//  Copyright © 2018年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
